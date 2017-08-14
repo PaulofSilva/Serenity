3c8494e3fba7a622aa10a5f285d18cb5f35a6f60
Paulo Silva <paulosilva@biglevel.pt>
2017-08-14 14:33:29 +0100

    Add project files.

---
+++ Serene/Serene.Web/Modules/Common/ScriptInitialization.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="../Common/Helpers/LanguageList.ts" />
+
+namespace Serene.ScriptInitialization {
+    Q.Config.responsiveDialogs = true;
+    Q.Config.rootNamespaces.push('Serene');
+    Serenity.EntityDialog.defaultLanguageList = LanguageList.getValue;
+}
